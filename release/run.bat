05450c8fb3c4425b5c225bc1d3ee9882d6246481
匆匆 <94792345+congcong999@users.noreply.github.com>
2023-06-12 06:06:11 +0800

    Delete run.bat

--- release/run.bat
+++
@@ -1,8 +0,0 @@
-move .\leanbackJava\release\*.apk .\
-move .\leanbackPython\release\*.apk .\
-move .\mobileJava\release\*.apk .\
-move .\mobilePython\release\*.apk .\
-rmdir /s /q .\leanbackJava
-rmdir /s /q .\leanbackPython
-rmdir /s /q .\mobileJava
-rmdir /s /q .\mobilePython
