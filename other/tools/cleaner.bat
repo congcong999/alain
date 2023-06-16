7ebe76147f455a5f078709902c15886baa5ef458
匆匆 <94792345+congcong999@users.noreply.github.com>
2023-06-17 04:09:38 +0800

    Add files via upload

--- other/tools/cleaner.bat
+++ other/tools/cleaner.bat
@@ -1,1 +1,6 @@
-
+git clone --mirror https://github.com/congcong999/alain.git
+java -jar bfg.jar --delete-files *.apk TV.git
+cd TV.git
+git reflog expire --expire=now --all && git gc --prune=now --aggressive
+git push
+git gc
